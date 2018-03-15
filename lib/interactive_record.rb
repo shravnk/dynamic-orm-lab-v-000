fc87ec562b9b08f31fd197785d8f074c3cc4429d
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-15 23:53:16 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,7 +51,7 @@ class InteractiveRecord
 
     DB[:conn].execute(sql)
 
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{self.table_name_for_insert}")[0][0]
   end
 
 end
